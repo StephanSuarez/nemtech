0781b628bf94d1699b408f1e43b574302b574f6f
Stephan Suarez = <stph@Stephans-MacBook-Pro.local>
2025-02-28 11:00:50 -0500

    abc

--- app/politics/page.tsx
+++ app/politics/page.tsx
@@ -9,8 +9,8 @@ export default function Politics() {
         <div className="max-w-5xl mx-auto py-16 px-4 relative z-10 bg-black text-white">
             
         <h2 className="text-4xl font-normal mb-8 uppercase">POLITICAS</h2>
-            {textPolitics.map((politics) => (
-        <Politica key={politics.title} politics={politics} />
+            {textPolitics.map((politics, index) => (
+        <Politica key={index} politics={politics} />
       ))}
         </div>
         </main>
